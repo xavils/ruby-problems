c65a9a2e6087cbc8362f635312ef916cdf593c55
Xavi Lozano Segarra <xavils@Xavis-MacBook-Air.local>
2015-05-05 10:32:56 +0800

    tripleDouble

---
+++ tripleDouble.rb
@@ -0,0 +1,33 @@
+#### Triple Double
+
+# Have the function TripleDouble(num1,num2) take both parameters being passed, and return 1 if there is a straight triple of a number at any place in num1 and also a straight double of the same number in num2.
+
+# For example: if num1 equals 451999277 and num2 equals 41177722899, then return 1 because in the first parameter you have the straight triple 999 and you have a straight double, 99, of the same number in the second parameter. If this isn't the case, return 0.
+
+def tripleDouble(triple,double)
+  # your magic
+  num1S = triple.to_s
+	num2S = double.to_s
+
+	if (num1S.length > 3) && (num2S.length > 2)
+	  i = 0
+
+	  while i < num1S.length-2 
+
+	    if (num1S[i] == num1S[i+1]) && (num1S[i+1] == num1S[i+2])
+
+	      if num2S.index(num1S[i]+ num1S[i+1]) != -1
+          puts 1
+	      else
+	      	puts 0
+	      end
+
+	    end
+	    i += 1
+	  end
+	else
+	  puts 0
+	end
+end
+
+puts tripleDouble(451999277, 41177722899)
